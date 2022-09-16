75561cc2ac3344439955f2ece20233f5ca0840a1
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-16 10:43:29 -0300

    add:percentage operation and equal function

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -21,6 +21,8 @@ class CalcController {
             this.setDisplayDateTime();
 
         }, 1000);
+
+        this.setLastNumberToDisplay();
     }
 
     addEventListenerAll(element, events, fn){
@@ -35,13 +37,17 @@ class CalcController {
 
     clearAll(){
 
-        this._operations = [];
+        this._operation = [];
+
+        this.setLastNumberToDisplay();
     
     }
 
     clearEntry(){
 
-        this._operations.pop();
+        this._operation.pop();
+
+        this.setLastNumberToDisplay();
 
     }
 
@@ -77,11 +83,29 @@ class CalcController {
 
     calc(){
 
-        let last = this._operation.pop();
+        let last ='';
+
+        if(this._operation.length > 3){
+
+            last = this._operation.pop();
+
+        }
 
         let result = eval(this._operation.join(""));
 
-        this._operation = [result, last];
+        if (last == "%"){
+
+            result /= 100;
+
+            this._operation = [result]
+
+        } else {
+
+            this._operation = [result];
+            
+            if (last) this._operation.push(last);
+
+        }
 
         this.setLastNumberToDisplay();
 
@@ -89,7 +113,7 @@ class CalcController {
 
     setLastNumberToDisplay(){
 
-        let lastNumber;
+             let lastNumber;
 
         for (let i = this._operation.length-1; i >= 0; i--){
 
@@ -103,7 +127,9 @@ class CalcController {
 
         }
 
-        this.displayCalc = lastNumber
+        if (!lastNumber) lastNumber = 0;
+
+        this.displayCalc = lastNumber;
 
     }
 
@@ -171,8 +197,8 @@ class CalcController {
                 this.addOperation('+');
                 break;
 
-                case 'subtracao':
-                    this.addOperation('-');
+            case 'subtracao':
+                this.addOperation('-');
                 break;
 
             case 'divisao':
@@ -188,7 +214,7 @@ class CalcController {
                 break;
 
             case 'igual':
-                this.addOperation('=');
+                this.calc('=');
                 break;
 
             case 'ponto':
