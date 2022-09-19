3f4395c5bcd3fc318f52c331948a10a4e1ceb995
v1n1c1u5ouza <viniciusdesouzamarinho@gmail.com>
2022-09-19 08:23:37 -0300

    Fix: Adding more than one dot

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -225,6 +225,8 @@ class CalcController {
 
         let lastOperation = this.getLastOperation();
 
+        if ( typeof lastOperation === 'string' && lastOperation.split('').indexOf('.' > -1)) return;
+
         if (this.isOperator(lastOperation) || !lastOperation) {
 
             this.pushOperation('0.');
